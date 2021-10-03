d72acab7cb1e25282468900e1d57badf00055daa
julius.suominen <julius.suominen@gmail.com>
2021-10-03 20:03:39 +0300

    add inital contract state

--- contracts/RentalAgreement.sol
+++ contracts/RentalAgreement.sol
@@ -1,7 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-contract RentalAgreement {
+contract Rentals {
+  
+  address public owner;
+
+  enum State{ Vacant, Rented, NotAvailable }
+
+  struct Property {
+    uint propertyId;
+    uint currentRentAmount;
+    State status;
+    address tenant;
+    Payment[] tenantPayments;
+  }
+
+  struct Payment {
+    uint date;
+    uint amount;
+    address from;
+  }
+
+  mapping (uint => Property) private properties;
+  mapping (uint => Payment) private payments;
 
   
 
